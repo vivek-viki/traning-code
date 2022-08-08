32a5318cf5b4dc3e48f385e402dab1c1cae27f69
vivek-viki <Mukunda.Vivek@ivycomptech.com>
2022-08-08 21:46:53 +0530

    Task4 done

---
+++ src/Task4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String args[])
+    {
+        int row, colunm, Number;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Row- ");
+        row=sc.nextInt();
+        System.out.print("Enter Colunm- ");
+        colunm=sc.nextInt();
+        System.out.print("Enter Number to find- ");
+        Number=sc.nextInt();
+
+        for(int i = 1; (i <= row*(colunm - 1)); i++ )
+        {
+            if (row * i == Number || row * (colunm - 1) == Number || Number <= row || (row * i) + 1 == Number) {
+                System.out.println("Is is an Mango tree");
+                break;
+            }
+        }
+
+    }
+}
