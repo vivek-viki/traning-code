4b50ea43706870a80c930de2e5f35c79e135bd62
vivek-viki <Mukunda.Vivek@ivycomptech.com>
2022-08-08 21:52:07 +0530

    Task4 changes made

--- src/Task4.java
+++ src/Task4.java
@@ -4,6 +4,7 @@ public class Task4 {
     public static void main(String args[])
     {
         int row, colunm, Number;
+        boolean value = false;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Row- ");
         row=sc.nextInt();
@@ -15,10 +16,15 @@ public class Task4 {
         for(int i = 1; (i <= row*(colunm - 1)); i++ )
         {
             if (row * i == Number || row * (colunm - 1) == Number || Number <= row || (row * i) + 1 == Number) {
-                System.out.println("Is is an Mango tree");
+                //System.out.println("Is is an Mango tree");
+                value = true;
                 break;
             }
         }
+    if(value)
+        System.out.println("Is is an Mango tree");
+    else
+        System.out.println("Is is not an Mango tree");
 
     }
 }
