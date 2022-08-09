6d6409e4954f8a4e8ef399a19d480997eef52646
vivek-viki <Mukunda.Vivek@ivycomptech.com>
2022-08-09 11:16:54 +0530

    one D array

---
+++ src/Arrayspgm.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Arrayspgm {
+        public static void main(String args[]) {
+            int array[] = {2, 4, 5, 6, 8, 10, 5, 6, 7, 1};
+
+                Arrays.sort(array);
+                int max = array[array.length - 1];
+                int min = array[0];
+                System.out.println(min);
+                System.out.println(max);
+        }
+}
