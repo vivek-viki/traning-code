8e1cf426eed4bb3598773574e08105fec0d6021b
@viki49081 <Mukunda.Vivek@ivycomptech.com>
2022-08-09 11:45:54 +0530

    moved to git lab

--- src/Arrayspgm.java
+++ src/Arrayspgm.java
@@ -1,7 +1,9 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Arrayspgm {
         public static void main(String args[]) {
+
             int array[] = {2, 4, 5, 6, 8, 10, 5, 6, 7, 1};
 
                 Arrays.sort(array);
